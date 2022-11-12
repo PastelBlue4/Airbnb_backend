75f8ddb91734a292ad1d8f8065544a27b1327773
Romuru <pastelblue0721@gmail.com>
2022-11-12 23:41:19 +0900

    [feat] test house model

---
+++ houses/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class House(models.Model):
+
+    name = models.CharField(max_length=140)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+
