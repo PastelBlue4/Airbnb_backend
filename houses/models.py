48d3877a4041db4b0fea0ba0d254f24fc3d9ebf5
Romuru <pastelblue0721@gmail.com>
2022-11-21 23:14:49 +0900

    [feat] ready for custom user model

--- houses/models.py
+++ houses/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+
 class House(models.Model):
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Posetive Numbers Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(default=True)
 
     def __str__(self):
-         return self.name
+        return self.name
