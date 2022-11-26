be3977656af4d526e73b916a8e54cb75f498dd07
Romuru <pastelblue0721@gmail.com>
2022-11-27 00:23:59 +0900

    [feat] more models and many to many something

--- rooms/models.py
+++ rooms/models.py
@@ -4,12 +4,52 @@ from django.db import models
 
 
 class Room(models.Model):
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Privateroom")
+        SHARED_ROOM = ("shared_room", "Shared Room")
 
     country = models.CharField(
         max_length=50,
         default="한국",
     )
+
     city = models.CharField(
         max_length=80,
         default="서울",
+    )
+
+    address = models.CharField(
+        max_length=250,
+    )
+
+    price = models.PositiveIntegerField()
+
+    rooms = models.PositiveIntegerField()
+
+    toilets = models.PositiveIntegerField()
+
+    pet_friendly = models.BooleanField(
+        default=True,
+    )
+
+    kind = models.CharField(
+        max_length=20,
+        choices=RoomKindChoices.choices,
+    )
+
+    description = models.TextField()
+
+
+class Amenity(models.Model):
+
+    """Amenity Definition"""
+
+    name = models.CharField(
+        max_length=150,
+    )
+
+    description = models.CharField(
+        max_length=150,
+        null=True,
     )
